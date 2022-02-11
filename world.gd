0320837f87c097622c7b603f1568f9c3f27d92b2
Theenell <theenell@outlook.com>
2022-02-11 20:54:00 +0100

    added function test in world.gd

--- world.gd
+++ world.gd
@@ -5,7 +5,8 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
+func test():
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
