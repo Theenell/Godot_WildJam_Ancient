06a8493799298f923f9955b70c99661035c2c14c
Theenell <theenell@outlook.com>
2022-02-11 22:55:42 +0100

    Projektfiler nollställda

--- src/Player/Aim_shape.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _draw():
-	draw_rect(Rect2(0.0, 0.0, 70.0, 5.0), ColorN("Yellow"), true)
--- src/objects/Moving_Platform.gd
+++
@@ -1,7 +0,0 @@
-extends PathFollow2D
-
-var speed = 100
-
-func _physics_process(delta):
-	position += speed * 1
-	
